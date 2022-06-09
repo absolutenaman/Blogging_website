324db4911160146479c7375fd92b4ba08bc885fc
Naman Srivastava <43997983+absolutenaman@users.noreply.github.com>
2022-06-09 23:04:10 +0530

    Port added for heroku

--- app.js
+++ app.js
@@ -96,7 +96,8 @@ app.get("/posts/:postId",async function(req,res)
  
 });
 
+const port= process.env.PORT || 3000;
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
